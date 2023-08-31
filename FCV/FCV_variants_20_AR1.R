849929599c70b50d28dfeef4943ad64ccc071ca4
huixu18 <143454645+huixu18@users.noreply.github.com>
2023-08-31 15:22:49 -0700

    Add files via upload

---
+++ FCV/FCV_variants_20_AR1.R
@@ -0,0 +1,134 @@
+##############################################
+# Compare different variants of FCV methods for ARMA(1,0) noise and n_test = 20
+##############################################
+library(ggplot2)
+library(ggpubr)
+source("helper.R")
+
+###########################
+# Run simulation for n_test = 5 and noise is ARMA(1,20)
+###########################
+nsim = 500
+n = 2000
+n_test = 20
+p = 20
+w = 80
+w_train = 40
+w_val = 20
+lag = 5
+alpha_list = c(0.1, 0.3, 0.5, 0.7, 0.9)
+for(alpha in alpha_list){
+  ## err_test: true test error 
+  ## err_fcv_cal: point estimator for QFCV 
+  ## [lo_fcv_cal, hi_fcv_cal]: predictive interval for QFCV 
+  ## err_fcv: point estimator for FCV 
+  ## se_fcv, se_fcv_cc, se_fcv_pi, se_fcv_pi_cc: se for naive FCV, FCV with covariance correction, FCV as predictive interval, FCV with covariance correction as predictive interval
+  err_test = err_fcv_cal = lo_fcv_cal = hi_fcv_cal = err_fcv = se_fcv = se_fcv_cc = se_fcv_pi = se_fcv_pi_cc = rep(NA, nsim)
+  for(i in 1:nsim){
+    data = data_gen(n, p, n_test, alpha = alpha)
+    err_test[i] = mean(fit(data$x[(n-w_train+1):n,], data$y[(n-w_train+1):n], data$x_test, data$y_test))
+    
+    ## QFCV predictive interval
+    result_fcv_cal = fcv_cal(n, data$x, data$y, w, w_train, w_val, lag)
+    err_fcv_cal[i] = result_fcv_cal$err_hat
+    lo_fcv_cal[i] = result_fcv_cal$err_hat_lo
+    hi_fcv_cal[i] = result_fcv_cal$err_hat_hi
+    
+    ## naive FCV confidence interval and predictive interval
+    err_fcv_list = fcv(n, data$x, data$y, w_train+n_test, w_train, n_test)
+    err_fcv[i] = mean(err_fcv_list)
+    se_fcv[i] = sd(err_fcv_list)/sqrt(length(err_fcv_list))
+    se_fcv_pi[i] = sd(err_fcv_list)
+    
+    ## FCV confidence and predictive interval with covariance correction
+    var_fcv = var(err_fcv_list)
+    m = floor(length(err_fcv_list) / 50) 
+    for(s in 1:m){
+      var_fcv = var_fcv + 2 * (1 - s/length(err_fcv_list)) * cov(err_fcv_list[1:(length(err_fcv_list)-s)], err_fcv_list[(s+1):(length(err_fcv_list))])
+    }
+    if(var_fcv < 0){
+      var_fcv = var(err_fcv_list)
+    }
+    se_fcv_cc[i] = sqrt(var_fcv/length(err_fcv_list))
+    se_fcv_pi_cc[i] = sqrt(var_fcv)
+    
+    cat("true test: ", err_test[i], "quantile: ", c(lo_fcv_cal[i], hi_fcv_cal[i]), fill = TRUE)
+  }
+  
+  out = list(err_test = err_test, err_fcv = err_fcv, se_fcv = se_fcv,
+             err_fcv_cal = err_fcv_cal, lo_fcv_cal = lo_fcv_cal, hi_fcv_cal = hi_fcv_cal,
+             err_fcv = err_fcv, se_fcv = se_fcv, se_fcv_cc = se_fcv_cc, se_fcv_pi = se_fcv_pi, se_fcv_pi_cc = se_fcv_pi_cc)
+  save(out, file = paste0("sparse_linear_5_AR1_alpha-", alpha,".RData"))
+  print(paste0("Results for alpha-", alpha, " saved to disk."))
+}
+
+###########################
+# Plot for n_test = 20 and noise is ARMA(1,0)
+###########################
+alpha_list = c(0.1,0.3,0.5,0.7,0.9)
+coverage_cal = c()
+length_cal = c()
+coverage_fcv = c()
+length_fcv = c()
+coverage_fcv_pi = c()
+length_fcv_pi = c()
+coverage_fcv_cc = c()
+length_fcv_cc = c()
+coverage_fcv_pi_cc = c()
+length_fcv_pi_cc = c()
+
+lo_length_cal = hi_length_cal = se_length_cal = se_length_fcv = se_length_fcv_cc = se_length_fcv_pi = se_length_fcv_pi_cc = c()
+for(alpha in alpha_list){
+  load(paste0("sparse_linear_20_AR1_alpha-", alpha,".RData"))
+  err_test = out$err_test
+  err_fcv_cal = out$err_fcv_cal
+  lo_fcv_cal = out$lo_fcv_cal
+  hi_fcv_cal = out$hi_fcv_cal
+  err_fcv = out$err_fcv
+  se_fcv = out$se_fcv
+  se_fcv_cc = out$se_fcv_cc 
+  se_fcv_pi = out$se_fcv_pi
+  se_fcv_pi_cc = out$se_fcv_pi_cc
+  length_base = quantile(err_test, 0.95) - quantile(err_test, 0.05)
+  coverage_cal <- c(coverage_cal, mean((err_test > lo_fcv_cal) & (err_test < hi_fcv_cal)))
+  length_cal <- c(length_cal, mean(hi_fcv_cal - lo_fcv_cal)/length_base)
+  coverage_fcv = c(coverage_fcv, mean((err_test > err_fcv - qnorm(0.95) * se_fcv) & (err_test < err_fcv + qnorm(0.95) * se_fcv)))
+  length_fcv <- c(length_fcv, 2 * qnorm(0.95) * mean(se_fcv)/length_base)
+  coverage_fcv_cc = c(coverage_fcv_cc, mean((err_test > err_fcv - qnorm(0.95) * se_fcv_cc) & (err_test < err_fcv + qnorm(0.95) * se_fcv_cc)))
+  length_fcv_cc = c(length_fcv_cc, 2 * qnorm(0.95) * mean(se_fcv_cc)/length_base)
+  coverage_fcv_pi = c(coverage_fcv_pi, mean((err_test > err_fcv - qnorm(0.95) * se_fcv_pi) & (err_test < err_fcv + qnorm(0.95) * se_fcv_pi)))
+  length_fcv_pi = c(length_fcv_pi, 2 * qnorm(0.95) * mean(se_fcv_pi)/length_base)
+  coverage_fcv_pi_cc = c(coverage_fcv_pi_cc, mean((err_test > err_fcv - qnorm(0.95) * se_fcv_pi_cc) & (err_test < err_fcv + qnorm(0.95) * se_fcv_pi_cc)))
+  length_fcv_pi_cc = c(length_fcv_pi_cc, 2 * qnorm(0.95) * mean(se_fcv_pi_cc)/length_base)
+  
+  se_length_cal <- as.vector(c(se_length_cal, sd(hi_fcv_cal - lo_fcv_cal)/length_base/sqrt(500)))
+  se_length_fcv <- as.vector(c(se_length_fcv, sd(2 * qnorm(0.95) * se_fcv)/length_base/sqrt(500)))
+  se_length_fcv_cc <- as.vector(c(se_length_fcv_cc, sd(2 * qnorm(0.95) * se_fcv_cc)/length_base/sqrt(500)))
+  se_length_fcv_pi <- as.vector(c(se_length_fcv_pi, sd(2 * qnorm(0.95) * se_fcv_pi)/length_base/sqrt(500)))
+  se_length_fcv_pi_cc <- as.vector(c(se_length_fcv_pi_cc, sd(2 * qnorm(0.95) * se_fcv_pi_cc)/length_base/sqrt(500)))
+}
+
+data <- data.frame(coverage = c(coverage_cal, coverage_fcv, coverage_fcv_cc, coverage_fcv_pi), alpha = rep(alpha_list, 4),
+                   length = c(length_cal, length_fcv, length_fcv_cc, length_fcv_pi),
+                   Method = rep(c("QFCV", "FCV", "FCV(c)", "FCV(p)"), each = length(coverage_cal)))
+p1 <- ggplot(data) + geom_line(aes(x = alpha, y = coverage, col = Method), size = 0.8) + geom_hline(yintercept=0.9, linetype="dashed", size = 1) + 
+  geom_ribbon(aes(ymin = coverage - qnorm(0.95) * sqrt(coverage * (1-coverage)/500), 
+                  ymax = coverage + qnorm(0.95) * sqrt(coverage * (1-coverage)/500),
+                  x = alpha, fill = Method), alpha = 0.2) +
+  theme_bw() + xlab("Autocorrelation") + ylab("Coverage") + theme(text = element_text(family = "Palatino", size = 16))
+p2 <- ggplot(data) + geom_line(aes(x = alpha, y = length, col = Method), size = 0.8) +
+  geom_ribbon(aes(ymin = length - qnorm(0.95) * c(se_length_cal, se_length_fcv, se_length_fcv_cc, se_length_fcv_pi),
+                  ymax = length + qnorm(0.95) * c(se_length_cal, se_length_fcv, se_length_fcv_cc, se_length_fcv_pi),
+                  x = alpha, fill = Method), alpha = 0.2) +
+  theme_bw() + xlab("Autocorrelation") + ylab("Length Ratio") + theme(text = element_text(family = "Palatino", size = 16))
+ggarrange(p1, p2, ncol=2, nrow=1, common.legend = TRUE)
+
+
+
+
+
+
+
+
+
+
--- FCV/helper.R
+++ FCV/helper.R
@@ -1,1 +1,103 @@
+##############################################
+# Helper functions for generating motivation plots
+##############################################
+library(glmnet)
+library(quantreg)
+require(doMC)
+registerDoMC(cores = 4)
+
+###########################
+# Data generation 
+###########################
+# Params: n = number of training samples, p = number of features, n_test = number of test samples, alpha = autocorrelation
+# Output: x = training data matrix, y = training outcome vector, x_test = test data matrix, y_test = test outcome vector 
+data_gen <- function(n, p, n_test, alpha = 0, MA = FALSE){
+  beta = rep(0, p)
+  beta[1:4] = 1
+  x = matrix(rnorm(n * p), nrow = n, ncol = p)
+  x_test = matrix(rnorm(n_test * p), nrow = n_test, ncol = p)
+  if(MA == FALSE){
+    noise = arima.sim(list(order = c(1,0,0), ar = alpha), n = n + n_test)
+  }
+  else{
+    noise = arima.sim(list(order = c(1,0,20), ar = alpha, ma = c(seq(10), rev(seq(10)))/10), n = n + n_test)/5
+  }
+  y = x %*% beta + noise[1:n]
+  y_test = x_test %*% beta + noise[(n+1):(n+n_test)]
+  return(list(x = x, y = y, x_test = x_test, y_test = y_test))
+}
+
+###########################
+# Model fitting and evaluation using glmnet
+###########################
+# Params: x_train = training data matrix, y_train = training outcome vector, x_test = test data matrix, y_test = test outcome vector
+# Output: err_list = vector of squared loss on test data
+fit <- function(x_train, y_train, x_test, y_test){
+  a=glmnet(x_train, y_train, family="gaussian", parallel = TRUE, lambda = 0.05)
+  y_hat=predict(a,x_test)
+  err_list = (y_hat - y_test)^2
+  return(err_list)
+}
+
+###########################
+# Forward cross-validation (FCV)
+###########################
+# Params: n = number of samples, x = data matrix, y = outcome vector, w = window size, w_train = window size for training, lag = FCV lag 
+# Output: err_val = vector of validation errors obtained from FCV 
+fcv <- function(n, x, y, w, w_train, lag){
+  start = 1
+  end = w 
+  nfold = floor((n-w)/lag) + 1
+  err_val = c()
+  for(fold in 1:nfold){
+    split = start + w_train - 1
+    err_val = c(err_val, mean(fit(x[start:split,], y[start:split], x[(split+1):end,], y[(split+1):end])))
+    start = start + lag
+    end = end + lag 
+  }
+  return(err_val)
+}
+
+###########################
+# Quantile-based forward cross-validation (QFCV)
+###########################
+# Params: n = number of samples, x = data matrix, y = outcome vector, w = window size, w_train = window size for training, w_val = window size for validation, lag = FCV lag
+# Output: err_val_list = vector of validation errors, err_test_list = vector of test errors, err_hat_lo = lower end of predictive interval, err_hat_hi = upper end of predictive interval, err_hat = point estimator for prediction error 
+fcv_cal <- function(n, x, y, w, w_train, w_val, lag){
+  w_test = w - w_train - w_val 
+  start = 1
+  end = w 
+  nfold = floor((n-w)/lag) + 1
+  err_val = c()
+  err_test = c()
+  for(fold in 1:nfold){
+    train_end = start + w_train - 1
+    val_start = start + w_train 
+    val_end = start + w_train + w_val - 1
+    test_start = start + w_train + w_val
+    err_val = c(err_val, mean(fit(x[start:train_end,], y[start:train_end], x[val_start:val_end,], y[val_start:val_end])))
+    err_test = c(err_test, mean(fit(x[(val_end - w_train + 1):val_end,], y[(val_end - w_train + 1):val_end], x[test_start:end,], y[test_start:end])))
+    start = start + lag
+    end = end + lag 
+  }
+  
+  val = fit(x[(n-w_train-w_val+1):(n-w_val),], y[(n-w_train-w_val+1):(n-w_val)], x[(n-w_val+1):n,], y[(n-w_val+1):n])
+  point_val = mean(val)
+  y_reg = err_test
+  x_reg = err_val
+  lr = lm(y_reg ~ x_reg)
+  err_hat = predict(lr, data.frame(x_reg = point_val))
+  
+  rqdata <- data.frame(err_test = err_test, err_val = err_val)
+  rqfit_lo <- rq(err_test ~ err_val, tau = 0.05, data = rqdata)
+  err_hat_lo = predict(rqfit_lo, newdata = data.frame(err_val = c(point_val)))
+  rqfit_hi <- rq(err_test ~ err_val, tau = 0.95, data = rqdata)
+  err_hat_hi = predict(rqfit_hi, newdata = data.frame(err_val = c(point_val)))
+  
+  return(list(err_val_list = err_val, err_test_list = err_test,
+              err_hat_lo = err_hat_lo, err_hat_hi = err_hat_hi, err_hat = err_hat))
+}
+
+
+
 
